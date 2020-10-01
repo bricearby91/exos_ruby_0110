e833460e2957cb658176d9a8bad64737d4371ee9
Arby Brice <brice.arby91@gmail.com>
2020-10-01 23:44:14 +0200

    ajoutdu dossier

---
+++ lib/00_journalists.rb
@@ -0,0 +1,162 @@
+tab = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def count(tab)
+nb=0
+tab.each do |element|
+	nb += 1
+end
+
+puts "Le tableau contient #{nb} éléments."
+end
+
+def shortest_handle(tab)
+min=100
+shortest_handle =""
+tab.each do |handle|
+	nb_char=0
+	handle.each_char do |char|
+		nb_char += 1
+	end
+	if nb_char < min
+		min = nb_char
+		shortest_handle = handle
+	end
+end
+puts "Le handle le plus court est #{shortest_handle}."
+end
+
+def longest_handle(tab)
+max=0
+longest_handle =""
+tab.each do |handle|
+	nb_char=0
+	handle.each_char do |char|
+		nb_char += 1
+	end
+	if nb_char > max
+		max = nb_char
+		longest_handle = handle
+	end
+end
+puts "Le handle le plus long est #{longest_handle} avec #{max} caractères."
+end
+
+def return_max(tab)
+max=0
+longest_handle =""
+tab.each do |handle|
+	nb_char=0
+	handle.each_char do |char|
+		nb_char += 1
+	end
+	if nb_char > max
+		max = nb_char
+		longest_handle = handle
+	end
+end
+return max
+end
+
+def five_char(tab)
+
+count = 0	
+tab.each do |handle|
+	nb_char=0
+	handle.each_char do |char|
+		nb_char += 1
+	end
+	if nb_char == 6
+		count += 1
+	end
+end
+
+puts "Il y a en tout #{count} handles constitués de cinq caractères."
+end
+
+def is_first_char_cap(tab)
+
+count=0
+tab.each do |handle|
+	char=handle[1]
+	cap=char.downcase
+	if cap != char
+		count += 1
+	end
+end
+
+puts "Il y a exactement #{count} handles dont le premier caractère est une majuscule."
+end
+
+def sort_alphabetically(tab)
+
+sorted_tab = tab.sort_by(&:downcase)
+
+puts "Liste des handles par ordre alphabétique : "
+puts sorted_tab
+end
+
+def sort_by_length(tab)
+
+sorted_tab = tab.sort_by(&:length)
+
+puts "Liste des handles par ordre de longueur : "
+puts sorted_tab
+end
+
+def search_handle(tab)
+print "De quel handle soutaitez-vous connaître l'index ? "
+handle=gets.chomp
+count = 0
+
+tab.each do |element|
+	if element == handle
+		break
+	else
+		count += 1
+	end
+end
+
+puts "Le handle #{handle} est situé à l'index #{count}."
+end
+
+
+def sort_by_length(tab)
+
+nb = []
+
+max_len=return_max(tab)
+
+max_len.times do |i|
+    nb << 0
+end
+
+tab.each do |element|
+        count=0
+        element.each_char do
+                count += 1
+        end
+	for i in (1...max_len)
+        if count == i
+          nb[i]+=1
+        end
+      end
+
+end
+
+puts "La liste contient :"
+for i in (1..max_len)
+  puts " > #{nb[i]} élément(s) contenant #{i} caractères"
+
+end
+end
+
+
+count(tab)
+shortest_handle(tab)
+longest_handle(tab)
+five_char(tab)
+is_first_char_cap(tab)
+#sort_alphabetically(tab)
+#sort_by_length(tab)
+search_handle(tab)
+sort_by_length(tab)
